578dc5759c80ea68650eb17751b13af6f42c8bd7
Rishi Rathore <118537512+rishirathoree@users.noreply.github.com>
2023-04-16 13:38:33 +0530

    Update Store.jsx

--- src/Store/Store.jsx
+++ src/Store/Store.jsx
@@ -1,7 +1,7 @@
 import { applyMiddleware, combineReducers, createStore } from "redux"
 import thunk from "redux-thunk"
 import { composeWithDevTools } from 'redux-devtools-extension';
-import { wishlistReducer } from "../Reducers/WIshlistreducer";
+import { wishlistReducer } from "../Reducers/Wishlistreducer";
 import { cartReducer } from "../Reducers/Cartreducer";
 import { productdisplayreducer } from "../Reducers/Productdisplayreducer";
 import { datareducer } from "../Reducers/Datareducer";
