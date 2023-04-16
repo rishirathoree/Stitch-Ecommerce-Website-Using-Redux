afc4cfe825afec45e7c7cc7918f548f2b8e73673
rishirathoree <rishirathoree3@gmail.com>
2023-04-16 08:03:21 +0000

    Update Store.jsx and tailwind.config.js

--- src/Store/Store.jsx
+++ src/Store/Store.jsx
@@ -1,24 +1,23 @@
-import { applyMiddleware, combineReducers, createStore } from "redux"
-import thunk from "redux-thunk"
-import { composeWithDevTools } from 'redux-devtools-extension';
-import { wishlistReducer } from "../Reducers/WIshlistreducer";
+import { applyMiddleware, combineReducers, createStore } from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { wishlistReducer } from "../Reducers/Wishlistreducer";
 import { cartReducer } from "../Reducers/Cartreducer";
 import { productdisplayreducer } from "../Reducers/Productdisplayreducer";
 import { datareducer } from "../Reducers/Datareducer";
 
-
 const reducer = combineReducers({
-    // add reducers over here
-    wishlist : wishlistReducer,
-    cart : cartReducer,
-    product : productdisplayreducer ,
-    data : datareducer
-})
-const Initialstate = []
-const middleware = [thunk]
+  // add reducers over here
+  wishlist: wishlistReducer,
+  cart: cartReducer,
+  product: productdisplayreducer,
+  data: datareducer,
+});
+const Initialstate = [];
+const middleware = [thunk];
 const store = createStore(
-    reducer,
-    Initialstate,
-    composeWithDevTools(applyMiddleware(...middleware))
-)
+  reducer,
+  Initialstate,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
 export default store;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       backgroundImage: {
-        'errorbg': "url('../images/monkey.png')",
+        errorbg: "url('../images/monkey.png')",
       },
       screens: {
-        'sm': '500px',
-        'md': '768px',
-        'lg': '1024px',
-        'xl': '1280px',
+        sm: "300px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
       },
     },
   },
   plugins: [],
-}
+};
